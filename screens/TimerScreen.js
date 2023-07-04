92f1e6ff8a6d23129dcf68eb296b5856f39d743f
Antoaane <antoine.lauzis@zetruc.fr>
2023-07-04 12:04:04 +0200

    adaptative time (hour, days...)

--- screens/TimerScreen.js
+++ screens/TimerScreen.js
@@ -54,9 +54,17 @@ const TimerScreen = () => {
         return () => cancelAnimationFrame(startRef.current);
     }, []);
 
+    const day = Math.floor(time / 86400000);
+    const hour = Math.floor(time / 3600000) % 24;
     const minutes = Math.floor(time / 60000) % 60;
     const seconds = Math.floor(time / 1000) % 60;
     const centiseconds = Math.floor(time / 10) % 100;
+
+    // const day = 2;
+    // const hour = 1;
+    // const minutes = 26;
+    // const seconds = 27;
+    // const centiseconds = 67;
 
 
     const [screenWidth, setScreenWidth] = useState(Dimensions.get('window').width);
@@ -111,15 +119,32 @@ const TimerScreen = () => {
             justifyContent: 'space-between',
         },
         min: {
-            fontSize: minutes > 9 ? 550 : 1100,
+            fontSize: 
+                minutes > 9 && hour < 1 && day < 1      ? 550 : 
+                hour > 9 && day < 1                     ? 550 : 
+                day > 9                                 ? 550 : 
+                                                        1100,
+
             fontFamily: 'AbrilFatface-Regular',
             color: theme === 'light' ? 'rgba(208, 134, 98, 0.2)' : 'rgba(255, 255, 255, 0.15)',
             textAlign: 'center',
             backgroundColor: 'transparent',
 
             position: 'absolute',
-            top: minutes > 9 ? screenHeight/20 : -screenHeight/2.75,
-            left: minutes > 19 ? -screenWidth/4 : minutes > 9 ? -screenWidth/6 : -screenWidth/4,
+            top: 
+                minutes > 9 && hour < 1 && day < 1      ? screenHeight/20 : 
+                hour > 9 && day < 1                     ? screenHeight/20 : 
+                day > 9                                 ? screenHeight/20 : 
+                                                        -screenHeight/2.75,
+
+            left: 
+                minutes > 19 && hour < 1                ? -screenWidth/4 : 
+                hour > 19 && day < 1                    ? -screenWidth/4 :
+                day > 19                                ? -screenWidth/4 :
+                minutes > 9 && hour < 1                 ? -screenWidth/6 : 
+                hour > 9 && day < 1                     ? -screenWidth/6 :
+                day > 9                                 ? -screenWidth/6 :
+                                                        -screenWidth/4,
 
             transform: [{ rotate: '20deg'}],
         },
@@ -138,15 +163,26 @@ const TimerScreen = () => {
             color: theme === 'light' ? 'rgba(208, 134, 98, 1)' : 'rgba(255, 255, 255, 1)'
         },
         minHorizontal: {
-            fontSize: minutes > 9 ? 500 : 650,
+            fontSize: 
+                minutes > 9 && hour < 1 && day < 1      ? 500 : 
+                hour > 9 && day < 1                     ? 500 : 
+                day > 9                                 ? 500 : 
+                                                        650,
             fontFamily: 'AbrilFatface-Regular',
             color: theme === 'light' ? 'rgba(208, 134, 98, 0.2)' : 'rgba(255, 255, 255, 0.15)',
             textAlign: 'center',
 
             position: 'absolute',
-            top: minutes > 9 ? -screenHeight /2 : -screenHeight/1.5,
-            left: minutes > 9 ? -screenWidth /30 : screenWidth/100,
-
+            top: 
+                minutes > 9 && hour < 1 && day < 1      ? -screenHeight/2 : 
+                hour > 9 && day < 1                     ? -screenHeight/2 : 
+                day > 9                                 ? -screenHeight/2 : 
+                                                        -screenHeight/1.5,
+            left: 
+                minutes > 9 && hour < 1 && day < 1      ? -screenWidth /30 : 
+                hour > 9 && day < 1                     ? -screenWidth /30 : 
+                day > 9                                 ? -screenWidth /30 : 
+                                                        screenWidth/100,
             transform: [{ rotate: '-20deg'}],
         },
         secHorizontal: {
@@ -163,6 +199,24 @@ const TimerScreen = () => {
 
             color: theme === 'light' ? 'rgba(208, 134, 98, 1)' : 'rgba(255, 255, 255, 1)'
         },
+        bonusTime: {
+            display: hour >= 1 ? 'flex' : 'none',
+
+            fontSize: 80,
+            fontFamily: theme === 'light' ? 'Lexend-Thin' : 'Product-Sans-Italic',
+            textAlign: 'center',
+        
+            color: theme === 'light' ? 'rgba(208, 134, 98, 1)' : 'rgba(255, 255, 255, 1)'
+        },
+        bonusTimeHorizontal: {
+            display: hour >= 1 ? 'flex' : 'none',
+
+            fontSize: 80,
+            fontFamily: theme === 'light' ? 'Lexend-Thin' : 'Product-Sans-Italic',
+            textAlign: 'center',
+
+            color: theme === 'light' ? 'rgba(208, 134, 98, 1)' : 'rgba(255, 255, 255, 1)'
+        },
     });
   
     return (
@@ -170,15 +224,18 @@ const TimerScreen = () => {
             <View style={styles.backGround}/>
             <View style={styles.container}>
                 <Text style={screenHeight > screenWidth ? styles.min : styles.minHorizontal}>
-                    {minutes}
+                    {day >= 1 ? day : hour >= 1 ? hour : minutes}
                 </Text>
                 <TouchableOpacity onPress={startTimer} onLongPress={resetTimer} style={styles.button}>
                     <View style={screenHeight > screenWidth ? styles.timer : styles.timerHorizontal}>
+                        <Text style={screenHeight > screenWidth ? styles.bonusTime : styles.bonusTimeHorizontal}>
+                            {day >= 1 ? seconds : hour >= 1 ? centiseconds : centiseconds}
+                        </Text>
                         <Text style={screenHeight > screenWidth ? styles.sec : styles.secHorizontal}>
-                            {seconds}
+                            {day >= 1 ? hour : hour >= 1 ? minutes : seconds}
                         </Text>
                         <Text style={screenHeight > screenWidth ? styles.cents : styles.centsHorizontal}>
-                            {centiseconds}
+                            {day >= 1 ? minutes : hour >= 1 ? seconds : centiseconds}
                         </Text>
                     </View>
                 </TouchableOpacity>
