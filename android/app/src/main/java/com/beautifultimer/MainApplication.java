04e4faa5cb71bff047f63449b349d8f7e5b77efa
Antoine Lauzis <antoine.lauzis@etudiant.univ-reims.fr>
2023-10-20 18:04:10 +0200

    feat: PLS WORK Ò_Ó

--- android/app/src/main/java/com/beautifultimer/MainApplication.java
+++ android/app/src/main/java/com/beautifultimer/MainApplication.java
@@ -1,4 +1,4 @@
-package com.bigtimer;
+package com.beautifultimer;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- ios/BeautifulTimerTests/BeautifulTimerTests.m
+++ ios/BeautifulTimerTests/BeautifulTimerTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface BigTimerTests : XCTestCase
+@interface BeautifulTimerTests : XCTestCase
 
 @end
 
-@implementation BigTimerTests
+@implementation BeautifulTimerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
